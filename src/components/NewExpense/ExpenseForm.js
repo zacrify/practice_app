2c2df8d5d263f27617780e8f02386dcaca8d4a18
pi <zacrify1986@gmail.com>
2023-05-19 13:48:14 +0100

    computed property(no idea what it is)

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,60 +1,64 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
-    // const [enteredTitle,setEnteredTitle] = useState('');
-    // const [enteredAmount,setEnteredAmount] = useState('');
-    // const [enteredDate,setEnteredDate] = useState('');
+function ExpenseForm() {
     const [userInput, setUserInput] = useState({
-        enteredTitle: '',
-        enteredAmount: '',
-        enteredDate: '',
+      enteredTitle: '',
+      enteredAmount: '',
+      enteredDate: '',
     });
+    const userInputChangeHandler = event => {
 
-    const titleChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredTitle: event.target.value,
-        });
+      setUserInput(prevUserInput => ({
+        ...prevUserInput,
+        [event.target.name]: event.target.value,
+      }));
+      console.log([event.target.value]);
     };
-
-    const amountChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredAmount: event.target.value,
-        });
-    };
-
-    const dateChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredDate: event.target.value,
-        });
-    };
-    
-  return (
-    <div>
+    console.log(userInput.enteredTitle);
+    return (
       <form>
         <div className="new-expense__controls">
           <div className="new-expense__control">
             <label>Title</label>
-            <input type="text" onChange={titleChangeHandler}/>
+            <input
+              type="text"
+              onChange={userInputChangeHandler}
+              name="enteredTitle"
+              value={userInput.enteredTitle}
+            />
           </div>
           <div className="new-expense__control">
             <label>Amount</label>
-            <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+            <input
+              type="number"
+              min="0.01"
+              step="0.01"
+              name="enteredAmount"
+              onChange={userInputChangeHandler}
+              value={userInput.enteredAmount}
+            />
           </div>
           <div className="new-expense__control">
             <label>Date</label>
-            <input type="date" min="2023-01-01" max="2024-01-01" onChange={dateChangeHandler}/>
+            <input
+              type="date"
+              min="2019-01-01"
+              max="2022-12-31"
+              name="enteredDate"
+              onChange={userInputChangeHandler}
+              value={userInput.enteredDate}
+            />
           </div>
         </div>
         <div className="new-expense__actions">
           <button type="submit">Add Expense</button>
         </div>
       </form>
-    </div>
-  );
-};
+    );
+  }
 
 export default ExpenseForm;
+//check this link for more understanding about property names in JS
+//https://eloquentcode.com/computed-property-names-in-javascript
+// https://dmitripavlutin.com/access-object-properties-javascript/
